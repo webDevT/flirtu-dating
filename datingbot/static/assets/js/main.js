33f54092a4a1386721f4c83960d9c3e8e8eff467
root <root@flirtu.io>
2021-05-05 20:00:08 +0000

    was added main.js

--- datingbot/static/assets/js/main.js
+++ datingbot/static/assets/js/main.js
@@ -1,3 +1,36 @@
+function localStorageTest(){
+    var test = 'test';
+    try {
+        localStorage.setItem(test, test);
+        localStorage.removeItem(test);
+        return true;
+    } catch(e) {
+        return false;
+    }
+}
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 $(function(){
 
 $('.cookis__setting').click(function(){
@@ -17,6 +50,8 @@ $('.cookis__setting-close').click(function(){
 
 $('.accept__cookis').click(function(){
 	$('.cookis-modal').fadeOut();
+	setCookie('__cp', "yes");
+	localStorage.setItem('__cp', "yes");
 })
 
 $('.cooking-setting-footer').click(function(){
@@ -52,8 +87,10 @@ $('.sub-menu-link').click(function(){
 })
 //-------end sticky header--------
 
-
-
-
+if (localStorageTest() && localStorage.getItem('__cp')) {
+	$('.cookis-modal').hide();
+} else if (navigator.cookieEnabled && getCookie('__cp')) {
+    $('.cookis-modal').hide();
+}
 
 });
