1b9efa1dda1fea97b6ab431dd12d3ae379a1c24a
karinadesyatnik <karinadesyatnik27@gmail.com>
2022-01-29 12:46:27 +0300

    seo optimisation

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "VIRTUAL_ENV=C:\Users\user\flirtu"
+set "VIRTUAL_ENV=D:\Python\projects\flirtu\flirtu-dating"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
